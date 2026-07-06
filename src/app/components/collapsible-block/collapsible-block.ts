de8dd9ac0db909e94ceef2ebbca656ab811842bf
Han <hans-lau2@hotmail.com>
2026-07-06 17:13:27 +0800

    feat: Update collapsible block to become controlled for exxpanded sstatae

--- src/app/components/collapsible-block/collapsible-block.ts
+++ src/app/components/collapsible-block/collapsible-block.ts
@@ -1,32 +1,25 @@
-import { Component, input, output, signal, ChangeDetectionStrategy, computed } from '@angular/core';
+import { Component, input, output, ChangeDetectionStrategy, computed } from '@angular/core';
 
 @Component({
   selector: 'app-collapsible-block',
   templateUrl: './collapsible-block.html',
   styleUrl: './collapsible-block.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: {
-    '[class.expanded]': 'isExpanded()',
-    '[class.collapsed]': '!isExpanded()',
-  }
 })
 export class CollapsibleBlock {
   title = input.required<string>();
   description = input.required<string>();
   imageUrl = input.required<string>();
   hash = input.required<string>();
+  isExpanded = input.required<boolean>();
   
+  toggle = output<void>();
   scrollToBlock = output<HTMLElement>();
   
-  protected isExpanded = signal(false);
   protected elementId = computed(() => `block-${this.hash()}`);
   
-  protected toggleExpanded(): void {
-    this.isExpanded.update(value => !value);
-  }
-  
-  protected onBlockClick(event: MouseEvent): void {
-    this.toggleExpanded();
+  protected onBlockClick(event: Event): void {
+    this.toggle.emit();
     const element = (event.currentTarget as HTMLElement);
     this.scrollToBlock.emit(element);
   }
